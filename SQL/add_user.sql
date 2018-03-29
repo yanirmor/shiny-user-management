a7146f6e84fea0b3fb33f97ffa8f5c022c5e733e
yanirmor <15572011+yanirmor@users.noreply.github.com>
2018-03-29 21:49:42 +0300

    queries

---
+++ SQL/add_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password_hash, sign_up, last_login)
+  VALUES (?username, ?password_hash, ?sign_up, ?last_login);
---
+++ SQL/update_last_login.sql
@@ -0,0 +1,3 @@
+UPDATE users 
+  SET last_login = ?last_login 
+  WHERE username = ?username;
